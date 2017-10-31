415cdebd6c7997d68ab7534167d263323ec95c5f
Mark Stacey <markjstacey@gmail.com>
2017-10-31 08:59:38 -0230

    Add RTM stuff

--- idlerpg-slack.py
+++ idlerpg-slack.py
@@ -1,4 +1,5 @@
 from slackclient import SlackClient
+import time
 
 slack_token = 'xoxb-264743104951-b9Irpl2oKKluOT7dzZFZxTB4' #os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
@@ -8,3 +9,13 @@ sc.api_call(
   channel="#general",
   text="Hello from Python! :tada:"
 )
+
+if sc.rtm_connect():
+    while True:
+        stuff = sc.rtm_read()
+
+        if (len(stuff) > 0):
+            print(len(stuff))
+else:
+    print("Connection Failed")
+
