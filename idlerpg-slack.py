b31cca59095a728f3d4e02ea2918d91b4e7c7ece
Mark Stacey <markjstacey@gmail.com>
2017-10-31 09:23:54 -0230

    Add dotenv support

--- idlerpg-slack.py
+++ idlerpg-slack.py
@@ -1,7 +1,13 @@
 from slackclient import SlackClient
-import time
+import time, os
 
-slack_token = 'xoxb-264743104951-b9Irpl2oKKluOT7dzZFZxTB4' #os.environ["SLACK_API_TOKEN"]
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+slack_token = os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
 
 sc.api_call(
